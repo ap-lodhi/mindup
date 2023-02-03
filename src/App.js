6830c33faec45ae45cf866cade59f11fed6b9e68
ap-lodhi <anilpatel.bcs@gmail.com>
2023-02-03 19:40:24 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+// import React, { useState } from 'react'
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
+
+function App() {
+  const { transcript, resetTranscript } = useSpeechRecognition({
+    continuous: true
+  });
+  // const [translate, setTranslate] = useState( [transcript])
+  let str = transcript;
+  console.log(str)
+ 
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
+  };
+  return (
+     <div>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+     
+      <p>{transcript}</p>
+<textarea id="story" name="story" value={transcript}
+          rows="5" cols="33" placeholder='text will be  printed here  '
+       
+         >
+          
+           
+
+</textarea>
+
+    </div>
+    
+  );
+}
+
+export default App;
